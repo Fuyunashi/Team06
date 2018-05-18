e105f71341ce20368216a46b55885506d3404039
Sim TH <sth4628@hotmail.com>
2018-05-18 09:26:32 +0900

    Enemy

---
+++ UnityProject/team6/Assets/W_Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W_Enemy : MonoBehaviour {
+
+    private Transform target;
+    private float accelaration;
+    private Vector3 targetPoint;
+    public float velocity= 5.0f;
+    public float accel;
+    public float detectionRange;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveToPlayer();
+	}
+    public void MoveToPlayer()
+    {
+        target = GameObject.Find("DummyPlayer").transform;
+        targetPoint = target.position - transform.position;
+        
+        accelaration = accel;
+        velocity += accelaration * Time.deltaTime;
+
+        if (targetPoint.sqrMagnitude <= detectionRange * detectionRange)
+        {
+            transform.LookAt(target);
+            transform.Translate(Vector3.forward * velocity);
+            Debug.Log("In");
+        }
+        else
+        {
+            velocity = 0.0f;
+            Debug.Log("Out");
+        }
+    }
+}
