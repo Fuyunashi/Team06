4e937b1e2b94ead39d1560c1371654b0b373fbaf
syotaueda <beat774boxer@gmail.com>
2018-06-05 11:46:21 +0900

    上田　select

--- UnityProject/team6/Assets/syota/Script/ChangeCamera.cs
+++ UnityProject/team6/Assets/syota/Script/ChangeCamera.cs
@@ -6,23 +6,22 @@ using Cinemachine;
 
 public class ChangeCamera : MonoBehaviour
 {
-
     [SerializeField]
     public CinemachineVirtualCamera[] v_camrea;
 
     GameObject obj_stageInstructs;
     StageInstructs stageInstructs;
- 
+
     void Start()
     {
         obj_stageInstructs = GameObject.Find("StageConfiguration");
         stageInstructs = obj_stageInstructs.GetComponent<StageInstructs>();
-   
-        v_camrea[0].Priority = 10;
-        for (int i = 0; i < v_camrea.Length; i++)
+
+        for (int i = 1; i < v_camrea.Length; i++)
         {
             v_camrea[i].Priority = 9;
         }
+        v_camrea[0].Priority = 11;
     }
 
     // Update is called once per frame
@@ -30,20 +29,36 @@ public class ChangeCamera : MonoBehaviour
     {
         if ((int)stageInstructs.CurrentStage >= 0 && (int)stageInstructs.CurrentStage <= 1)
         {
-            v_camrea[1].Priority = 11;
+            v_camrea[1].Priority = 12;
+            v_camrea[2].Priority = 10;
+            v_camrea[3].Priority = 10;
+            v_camrea[4].Priority = 10;
+
         }
         if ((int)stageInstructs.CurrentStage >= 2 && (int)stageInstructs.CurrentStage <= 7)
         {
-            v_camrea[2].Priority = 11;
+            v_camrea[2].Priority = 12;
+
+            v_camrea[1].Priority = 10;
+            v_camrea[3].Priority = 10;
+            v_camrea[4].Priority = 10;
         }
         if ((int)stageInstructs.CurrentStage >= 8 && (int)stageInstructs.CurrentStage <= 13)
         {
-            v_camrea[3].Priority = 11;
+            v_camrea[3].Priority = 12;
+
+            v_camrea[1].Priority = 10;
+            v_camrea[2].Priority = 10;
+            v_camrea[4].Priority = 10;
         }
         if ((int)stageInstructs.CurrentStage >= 14 && (int)stageInstructs.CurrentStage <= 19)
         {
-            v_camrea[4].Priority = 11;
-        }         
-       
+            v_camrea[4].Priority = 12;
+
+            v_camrea[1].Priority = 10;
+            v_camrea[2].Priority = 10;
+            v_camrea[3].Priority = 10;
+        }
+
     }
 }
