058e1e3777be0f51c5de47076d5b97b7ff0a2b38
fuyunashi <s.daiki.0816@gmail.com>
2018-06-10 14:04:41 +0900

    model

---
+++ UnityProject/team6/Assets/Shindo/Scripts/modeldirection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modeldirection : MonoBehaviour {
+
+    [SerializeField]
+    Camera camera_;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        this.transform.forward = camera_.transform.forward;
+	}
+}
