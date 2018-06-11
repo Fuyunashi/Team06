a02820afe4b0be23ee67fd326447c11cd549386a
Sim TH <sth4628@hotmail.com>
2018-06-12 03:31:18 +0900

    Enemy, Laser Sound

---
+++ UnityProject/team6/Assets/Sim/Scripts/soundManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour {
+    public AudioClip enemyDead;
+    public AudioClip enemyToPlayer;
+    public AudioClip laserHit;
+
+    AudioSource simAudio; 
+    public static soundManager instance; 
+
+    void Awake()  
+    {
+        if (soundManager.instance == null)  
+            soundManager.instance = this;  
+    }
+    // Use this for initialization
+    void Start()
+    {
+        simAudio = GetComponent<AudioSource>();  
+    }
+
+    public void EnemyDead()
+    {
+        simAudio.PlayOneShot(enemyDead);
+    }
+    public void EnemyToPlayer()
+    {
+        simAudio.PlayOneShot(enemyToPlayer);
+    }
+    public void LaserHit()
+    {
+        simAudio.PlayOneShot(laserHit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
